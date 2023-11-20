5c935949aca4ee0dfaa1a1e8469208fcc0e31999
unknown <s6404062620109@email.kmutnb.ac.th>
2023-11-21 01:20:57 +0700

    add login, getuserdata, getbuildingh3 api.

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const moment = require('moment-timezone');
+const jwt = require('jsonwebtoken');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST","PUT","DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 const db = mysql.createConnection({
@@ -114,6 +119,19 @@ app.get('/getbuilding', (req, res) =>{
     });
 });
 
+app.get('/getbuildingh3', (req, res) =>{
+    db.query("SELECT * FROM building WHERE building = 'H3' ORDER BY building", (err, result) =>{
+        if(err){
+            console.log(err);
+            res.status(500).json({ message: "Cann't connect building table" });
+        }
+        else{
+            // console.log("Get result success!");
+            res.status(200).json({ result });
+        }
+    });
+});
+
 app.get('/getencoderInput', (req, res) => {
     db.query(`SELECT encoder_input.no, encoder_input.status,
     encoder_input.dir, encoder_input.event, encoder_input.ip, 
@@ -139,14 +157,61 @@ app.post('/getbuildingsensor', (req, res) => {
     WHERE building.building=? ORDER BY building.number `, [building], (err, result) => {
         if(err){
             console.log(err);
-            res.status(500).json({ message: "Cann't connect e   ncoder_input/building table" });
+            res.status(500).json({ message: "Cann't connect encoder_input/building table" });
         }
         else{
             // console.log("Get result success!");
             res.status(200).json({ result });
         }
-    })
-})
+    });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    db.query(`SELECT * FROM user WHERE username=? AND password=?`, 
+    [username, password], (err, result) => {
+        if(err){
+            console.log(err);
+            res.status(500).json({ message: "Cann't connect user table" });
+        }
+        else{
+            // console.log(result);
+            if(result.length>0){
+                // res.status(200).json({ result });
+                const token = jwt.sign({username}, "accessToken", {expiresIn: '1h'});
+                const decoded = jwt.verify(token, "accessToken");
+                const expired_date = new Date(decoded.exp * 1000);
+
+                db.query(`UPDATE user SET token = ?, iat = NOW() ,exp = ? WHERE username = ?`,
+                [ token, expired_date, username ], (err, result) => {
+                    if(err){
+                        console.log(err);
+                        res.status(500).json({ message: "Cann't connect user table" });
+                    }
+                    else{
+                        return res.json({message: "SignIn Successful!",token:token , exp:expired_date})
+                    }
+                });
+            }
+            else{res.status(200).json({ message: "Invalid password" });}
+        }
+    });
+});
+
+app.post('/getuserdata', (req, res) => {
+    const token = req.body.token;
+    db.query(`SELECT * FROM user WHERE token = ? AND exp > NOW()`,
+    [token], (err, result) => {
+        if(err){
+            console.log(err);
+            res.status(500).json({ message: "Cann't connect user table" });
+        }
+        else{
+            res.status(200).json({ result });
+        }
+    });
+});
 
 app.listen('3001', () =>{
     console.log('Server is running on port 3001');
